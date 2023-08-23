8cf3b068fdf3a6da138f6606e2b1afba7375c0e6
Kamonsith R <kamonsith.r@gmail.com>
2023-08-23 14:16:14 +0700

    Add Book model

---
+++ models/book.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Author ',
+    required: true
+  },
+  summary: {
+    type: String,
+    required: true
+  },
+  isbn: {
+    type: String,
+    required: true
+  },
+  genre: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Genre '
+  }]
+})
+
+schema.virtual('url')
+  .get(function () {
+    return `/catalog/book/${this._id}`
+  })
+
+module.exports = mongoose.model('Book', schema)
