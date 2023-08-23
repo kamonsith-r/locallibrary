e79263e9dece047109bce29095f94c8a9e65fef3
Kamonsith R <kamonsith.r@gmail.com>
2023-08-23 14:10:23 +0700

    Connect to database

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 require('dotenv').config()
 const express = require('express')
 const createError = require('http-errors')
+const mongoose = require('mongoose')
 const app = express()
-const { PORT = 8000 } = process.env
+const { PORT = 8000, DBUSERNAME, DBPASSWORD, DBNAME } = process.env
+
+mongoose.connect(`mongodb+srv://${DBUSERNAME}:${DBPASSWORD}@cluster0.1djg9yn.mongodb.net/${DBNAME}?retryWrites=true&w=majority`)
+  .then(connected => console.info('✔️  Connected to database:', connected.connections[0].name))
+  .catch(err => console.error('❗ Cannot connect to database:', err.message))
 
 app.set('view engine', 'ejs')
 app.use(express.json())
