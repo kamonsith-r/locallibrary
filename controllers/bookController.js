b92064d117ad341960e5cf15ce08d6e537645d05
Kamonsith R <kamonsith.r@gmail.com>
2023-08-25 15:22:23 +0700

    Modify catalog page to send summary information

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,8 +1,22 @@
 const Book = require('../models/book')
+const Author = require('../models/author')
+const Genre = require('../models/genre')
+const BookInstance = require('../models/bookinstance')
 const asyncHandler = require('express-async-handler')
 
 exports.index = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Site Home Page')
+  const [bookCount, bookInstanceCount, bookInstanceAvailableCount, authorCount, genreCount]
+    = await Promise.all([
+      Book.countDocuments({}).exec(),
+      BookInstance.countDocuments({}).exec(),
+      BookInstance.countDocuments({ status: 'Available' }).exec(),
+      Author.countDocuments({}).exec(),
+      Genre.countDocuments({}).exec(),
+    ])
+  return res.json({
+    title: 'Local Library Home',
+    bookCount, bookInstanceCount, bookInstanceAvailableCount, authorCount, genreCount
+  })
 })
 
 exports.book_list = asyncHandler(async (req, res, next) => {
