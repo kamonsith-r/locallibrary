2f1f71c9c5f730b6c0e881a00b1913712897c25a
Kamonsith R <kamonsith.r@gmail.com>
2023-08-25 12:24:43 +0700

    Add BookInstance model

---
+++ models/bookinstance.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema({
+  book: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Book',
+    required: true
+  },
+  imprint: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: String,
+    required: true,
+    enum: ['Available', 'Maintenance', 'Loaned', 'Reserved'],
+    default: 'Maintenance'
+  },
+  due_back: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+schema.virtual('url')
+  .get(function () {
+    return `/catalog/bookinstance/${this._id}`
+  })
+
+module.exports = mongoose.model('BookInstance', schema)
