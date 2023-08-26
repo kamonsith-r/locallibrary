aeed874b57dafeae7045966588c946dab6a3efde
Kamonsith R <kamonsith.r@gmail.com>
2023-08-26 12:38:17 +0700

    Modify book route to create new book

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,8 +1,9 @@
+const asyncHandler = require('express-async-handler')
+const { body, validationResult } = require('express-validator')
 const Book = require('../models/book')
 const Author = require('../models/author')
 const Genre = require('../models/genre')
 const BookInstance = require('../models/bookInstance')
-const asyncHandler = require('express-async-handler')
 
 exports.index = asyncHandler(async (req, res, next) => {
   const [bookCount, bookInstanceCount, bookInstanceAvailableCount, authorCount, genreCount]
@@ -44,12 +45,36 @@ exports.bookDetail = asyncHandler(async (req, res, next) => {
 })
 
 exports.bookCreateGET = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Book create GET')
+  const [authors, genres] = await Promise.all([Author.find(), Genre.find()])
+  return res.json({ title: 'Create Book', authors, genres })
 })
 
-exports.bookCreatePOST = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Book create POST')
-})
+exports.bookCreatePOST = [
+  (req, res, next) => {
+    if (!(req.body.genre instanceof Array)) {
+      if (typeof req.body.genre === 'undefined') req.body.genre = []
+      else req.body.genre = new Array(req.body.genre)
+    }
+    next()
+  },
+  body('title', 'Title must not be empty.').trim().isLength({ min: 1 }).escape(),
+  body('author', 'Author must not be empty.').trim().isLength({ min: 1 }).escape(),
+  body('summary', 'Summary must not be empty.').trim().isLength({ min: 1 }).escape(),
+  body('isbn', 'ISBN must not be empty').trim().isLength({ min: 1 }).escape(),
+  body('genre.*').escape(),
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req)
+    const book = new Book(req.body)
+    if (!errors.isEmpty()) {
+      const [authors, genres] = await Promise.all([Author.find(), Genre.find()])
+      for (const genre of genres) {
+        if (book.genre.includes(genre._id)) genre.checked = 'true'
+      }
+      return res.json({ title: 'Create Book', authors, genres, book, errors: errors.array() })
+    }
+    await book.save()
+    return res.redirect(book.url)
+  })]
 
 exports.bookDeleteGET = asyncHandler(async (req, res, next) => {
   res.send('NOT IMPLEMENTED: Book delete GET')
