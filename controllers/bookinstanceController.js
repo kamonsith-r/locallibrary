fdc14354cabaa35138e7a93a56c7572dceb07748
Kamonsith R <kamonsith.r@gmail.com>
2023-08-26 19:34:35 +0700

    Modify bookInstance routes for delete and update

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -22,24 +22,36 @@ exports.bookInstanceDetail = asyncHandler(async (req, res, next) => {
 })
 
 exports.bookInstanceCreateGET = asyncHandler(async (req, res, next) => {
-  const bookList = await Book.find().select('title')
+  const bookList = await Book.find()
+    .select('title')
   return res.json({ title: 'Create BookInstance', bookList })
 })
 
 exports.bookInstanceCreatePOST = [
-  body('book', 'Book must be specified').trim().isLength({ min: 1 }).escape(),
-  body('imprint', 'Imprint must be specified').trim().isLength({ min: 1 }).escape(),
-  body('status').escape(),
-  body('due_back', 'Invalid date').optional({ values: 'falsy' }).isISO8601().toDate(),
+  body('book', 'Book must be specified')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('imprint', 'Imprint must be specified')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('status')
+    .escape(),
+  body('due_back', 'Invalid date')
+    .optional({ values: 'falsy' })
+    .isISO8601()
+    .toDate(),
   asyncHandler(async (req, res, next) => {
     const errors = validationResult(req)
     const bookInstance = new BookInstance(req.body)
     if (!errors.isEmpty()) {
-      const bookList = await Book.find().select('title')
+      const bookList = await Book.find()
+        .select('title')
       return res.json({
         title: 'Create BookInstance',
-        selectedBook: bookInstance.book?._id,
         errors: errors.array(),
+        selectedBook: bookInstance.book?._id,
         bookList, bookInstance
       })
     }
@@ -48,17 +60,56 @@ exports.bookInstanceCreatePOST = [
   })]
 
 exports.bookInstanceDeleteGET = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: BookInstance delete GET')
+  const bookInstance = await BookInstance.findById(req.params.id)
+    .populate('book')
+  if (bookInstance === null) {
+    return res.redirect('/catalog/bookinstances')
+  }
+  return res.json({ title: 'Delete BookInstance', bookInstance })
 })
 
 exports.bookInstanceDeletePOST = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: BookInstance delete POST')
+  await BookInstance.findByIdAndRemove(req.body.id)
+  return res.redirect('/catalog/bookinstances')
 })
 
 exports.bookInstanceUpdateGET = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: BookInstance update GET')
+  const [bookInstance, bookList] = await Promise.all([
+    BookInstance.findById(req.params.id)
+      .populate('book'),
+    Book.find()
+  ])
+  if (bookInstance === null) {
+    const err = new Error('Book copy not found')
+    err.status = 404
+    return next(err)
+  }
+  return res.json({ title: 'Update Book Instance', selectedBook: bookInstance.book?._id, bookList, bookInstance })
 })
 
-exports.bookInstanceUpdatePOST = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: BookInstance update POST')
-})
+exports.bookInstanceUpdatePOST = [
+  body('book', 'Book must be specified')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('imprint', 'Imprint must be specified')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('status')
+    .escape(),
+  body('due_back', 'Invalid date')
+    .optional({ values: 'falsy' })
+    .isISO8601()
+    .toDate(),
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req)
+    const bookInstance = new BookInstance(req.body)
+    bookInstance._id = req.params.id
+    if (!errors.isEmpty()) {
+      const bookList = await Book.find().select('title')
+      return res.json({ title: 'Update BookInstance', errors: errors.array(), selectedBook: bookInstance.book?._id, bookList, bookInstance })
+    }
+    await BookInstance.findByIdAndUpdate(req.params.id, bookInstance)
+    return res.redirect(bookInstance.url)
+  })]
